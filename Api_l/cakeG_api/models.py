46a0734ca47bbdd4dac12ba8372dc46886f16882
Nabin <nabinpauudel664@gmail.com>
2022-03-21 00:42:23 +0545

    lol

--- Api_l/cakeG_api/models.py
+++ Api_l/cakeG_api/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Occation(models.Model):
+
+    name = models.CharField(_(""), max_length=50)
+
+    class Meta:
+        verbose_name = _("occation")
+        verbose_name_plural = _("occations")
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("occation_detail", kwargs={"pk": self.pk})
+
+
+
+class All_cakes(models.Model):
+    name = models.CharField(_(""), max_length=50)
+    occation = models.ForeignKey("Occation", verbose_name=_(""), on_delete=models.CASCADE)
